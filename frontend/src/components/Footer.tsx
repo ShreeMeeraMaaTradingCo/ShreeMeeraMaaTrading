50bd89fac360e6106eb612d0920e88c02b8c8011
Vidhi Soni <vidhisoni924@gmail.com>
2026-05-25 19:17:07 +0530

    Frontend changes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ import location from "../assets/location.png"
 import mail from "../assets/email.png"
 import phone from "../assets/phone.png"
 import id from "../assets/id-card.png"
-
+import logo from "../assets/footer-logo.png";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
@@ -15,7 +15,7 @@ export default function Footer() {
       <div className="footer-container">
         <div className="footer-grid">
           <div className="footer-section">
-            <h3 className="footer-title">Your Business</h3>
+            <img src={logo} alt="Business Logo" className="footer-logo"/>
             <p className="footer-description">
               Providing quality products and exceptional service to our customers since 2022.
             </p>
@@ -24,10 +24,10 @@ export default function Footer() {
           <div className="footer-section">
             <h4 className="footer-heading">Contact Info</h4>
             <ul className="footer-contact">
-              <li><img src={id} alt="Owner" /> SARTHAK AGRAWAL</li>
+              <li><img src={id} alt="Owner" className="owner"/> SARTHAK AGRAWAL</li>
               <li><img src={owner} alt="Mail" /> Proprietor</li>
               <li><img src={phone} alt="Phone" /> +91 9009220308</li>
-              <li><img src={mail} alt="Mail" /> shreemeeramaatrading@gmail.com</li>
+              <li><img src={mail} alt="Mail" className="owner" /> shreemeeramaatrading@gmail.com</li>
               <li><img src={location} alt="Location" /> Warehouse No. 27, New Anaj Mandi, Chhawani, Indore - 452001, India</li>
             </ul>
           </div>
